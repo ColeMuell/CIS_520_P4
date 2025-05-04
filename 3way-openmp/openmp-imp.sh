6149e358071c8dbf197261841843ba4f62416a45
Jack <jwschaef@ksu.edu>
2025-05-04 18:39:03 -0500

    Got the OPENMPI scriipts to give the right result

--- 3way-openmp/openmp-imp.sh
+++ 3way-openmp/openmp-imp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -l
 
- export OMP_NUM_THREADS=$1
-mpirun -x OMP_NUM_THREADS=1 ./3way-openmp/openmp-imp ~dan/625/wiki_dump.txt mdrun -nsteps 500000 -ntomp 1 -v -deffnm 1ns -c 1ns.pdb -nice 0
+export OMP_NUM_THREADS=$1
+./3way-openmp/openmp-imp ~dan/625/wiki_dump.txt 
+./3way-openmp/openmp-imp ~dan/625/wiki_dump.txt 
+./3way-openmp/openmp-imp ~dan/625/wiki_dump.txt 
+./3way-openmp/openmp-imp ~dan/625/wiki_dump.txt 
+./3way-openmp/openmp-imp ~dan/625/wiki_dump.txt 
