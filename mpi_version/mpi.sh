ce3b17b0ca05e6fff5833da7f554d931aca9b283
Tatenda Sekabanja <sekabanj@icr-helios.beocat.ksu.edu>
2025-05-04 22:40:33 -0500

    m

---
+++ mpi_version/mpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+
+#SBATCH --job-name=mpi_p4
+#SBATCH --time=0-1:00:00
+#SBATCH --ntasks-per-node=4
+#SBATCH --nodes=4
+#SBATCH --mem=2G
+##SBATCH --constraint=moles
+
+module load foss
+module load CMake/3.23.1-GCCcore-11.3.0 foss/2022a OpenMPI/4.1.4-GCC-11.3.0 CUDA/11.7.0
+
+
+mpirun -n 11 /homes/sekabanj/CIS_520_P4/mpi_version/mpi_version
